1176bd4af0dbd58537e303e2d682946cbf6a22ae
Nicolas369 <nicolas.gabrenas@gmail.com>
2022-07-05 23:13:23 -0300

    add format date in location-card

--- src/pages/locations/components/LocationCard.js
+++ src/pages/locations/components/LocationCard.js
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { getCharactersById } from '../../../state/queriesSlice';
+import { getCharactersById } from "../../../state/queriesSlice";
 import { Link } from "@tanstack/react-location";
 
 function LocationCard({ location }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const redirectToCharacters = (ids) => {
-        dispatch(getCharactersById({ ids: ids.map(resident => resident.id) }));
-    }
+  const redirectToCharacters = (ids) => {
+    dispatch(getCharactersById({ ids: ids.map((resident) => resident.id) }));
+  };
+
+  const getDate = () => {
+    const date = new Date(location.created);
+
+    const year = date.getFullYear();
+
+    const month =
+      date.getMonth().toString().length < 2
+        ? `0${date.getMonth()}`
+        : date.getMonth()
+    ;
+
+    const day =
+      date.getDay().toString().length < 2 
+        ? `0${date.getDay()}` 
+        : date.getDay()
+    ;
+
+    return `${year}/${month}/${day}`;
+  };
 
   return (
     <Card>
@@ -18,23 +38,26 @@ function LocationCard({ location }) {
 
       <CardContent>
         <ul>
-            <li>
-                <h3>Type: {location.type}</h3>
-            </li>
-            <li>
-                <h3>
-                    Dimension: <br />
-                    {location.dimension}
-                </h3>
+          <li>
+            <h3>Type: {location.type}</h3>
           </li>
-            <li>
-                <h3>Created: {location.created}</h3>
-            </li>
-            <li >
-                <LinkToCharacters onClick={() => redirectToCharacters(location.residents)} to={'/characters'} > 
-                    residents ⫸ 
-                </LinkToCharacters>
-            </li>
+          <li>
+            <h3>
+              Dimension: <br />
+              {location.dimension}
+            </h3>
+          </li>
+          <li>
+            <h3>Created: {getDate()}</h3>
+          </li>
+          <li>
+            <LinkToCharacters
+              onClick={() => redirectToCharacters(location.residents)}
+              to={"/characters"}
+            >
+              residents ⫸
+            </LinkToCharacters>
+          </li>
         </ul>
       </CardContent>
     </Card>
@@ -76,49 +99,17 @@ const CardContent = styled.div`
 `;
 
 const LinkToCharacters = styled(Link)`
-    width: fit-content;
-    height: fit-content;
-    font-size: 18px;
-    font-weight: 700;
-    padding: 7px;
-    border: 2px solid #50FF00;
-    text-decoration: none;
-    color:#50FF00;
+  width: fit-content;
+  height: fit-content;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 7px;
+  border: 2px solid #50ff00;
+  text-decoration: none;
+  color: #50ff00;
 
-
-    &:hover {
-        background-color: #50FF00;
-        color:#000000;
-    }
+  &:hover {
+    background-color: #50ff00;
+    color: #000000;
+  }
 `;
-
-// .card {
-//     height: fit-content;
-//     width: 350px;
-//     border-radius: 20px;
-//     display: flex;
-//     flex-direction: column;
-//     margin: 25px;
-//     border: 3px solid #008F11;
-//     box-shadow: 0px 0px 20px 3px #005200;
-//     background-color: #003300;
-
-//     &__title {
-//         height: 100px;
-//         width: 100%;
-//         padding: 15px;
-//         box-sizing: border-box;
-//         border: solid #008F11;
-//         border-width: 0px 0px 3px 0px;
-//         display: flex;
-//         align-items: center;
-//         justify-content: center;
-//     }
-
-//     &__content {
-//         font-size: 18px;
-//         line-height: 30px;
-//         margin: 10px;
-//     }
-
-    
